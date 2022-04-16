7155844a019f520d54400d95b9aeb8f73d9c63fa
Oleksandr Panov <oleksandr.panov@styrax.cz>
2022-04-16 21:00:11 +0200

    make plus animation

--- app.js
+++ app.js
@@ -44,10 +44,11 @@ function dr2(deg) {
 }
 
 function btnClick(e) {
-    func2('#0000ff');
-    func2('#00ff00');
-    func2('#ffffff');
-    func2('#ff0000');
+    dr3();
+    // func2('#0000ff');
+    // func2('#00ff00');
+    // func2('#ffffff');
+    // func2('#ff0000');
 }
 
 async function dr3(x = 100, y = 100) {
@@ -58,17 +59,110 @@ async function dr3(x = 100, y = 100) {
         if (colorI >= colors.length) {
             colorI = 0;
         }
-        return color;
+        return colors[colorI];
     }
     console.log('start');
-    await makePlus(ctx, color, x, y);
+    await makePlus(ctx, getColor(), x, y);
     console.log('end');
 }
 
 function makePlus(ctx, color, x = 100, y = 100, width = 200, height = 200) {
     return new Promise((resolve, reject) => {
-        //
+        const ANIMATION_DURATION = 2000;
+        
+        const shifts = makeShifts(true);
+        console.log(shifts);
+
+        const topLeftImgData = ctx.getImageData(0, 0, x, y);
+        const topRightImgData = ctx.getImageData(x, 0, width - x, y);
+        const bottomLeftImgData = ctx.getImageData(0, y, x, height - y);
+        const bottomRightImgData = ctx.getImageData(x, y, width - x, height - y);
+
+        let start = 0;
+        function stepAction(time) {
+            const realShifts = makeShiftsWithTime(shifts, time, ANIMATION_DURATION);
+            console.log(realShifts);
+            ctx.clearRect(0, 0, 200, 200);
+            ctx.fillStyle = color;
+            ctx.fillRect(0, 0, width, height);
+            ctx.putImageData(topLeftImgData, -realShifts.top.left, -realShifts.left.top);
+            ctx.putImageData(topRightImgData, x + realShifts.top.right, -realShifts.right.top);
+            ctx.putImageData(bottomLeftImgData, -realShifts.bottom.left, y + realShifts.left.bottom);
+            ctx.putImageData(bottomRightImgData, x + realShifts.bottom.right, y + realShifts.right.bottom);
+        }
+        function onEnd() {
+            resolve(true);
+        }
+
+        function step(timestamp) {
+            if (!start) {
+                start = timestamp;
+            }
+            const elapsed = timestamp - start;
+            if (elapsed >= ANIMATION_DURATION) {
+                stepAction(ANIMATION_DURATION);
+                onEnd();
+            } else {
+                stepAction(elapsed);
+                window.requestAnimationFrame(step);
+            }
+        }
+
+        window.requestAnimationFrame(step);
     });
+}
+
+function makeShifts(equal = false) {
+    const min = 5;
+    const max = 5;
+    function getRandom() {
+        return map(Math.random(), 0, 1, min, max);
+    }
+    const oneRandom = getRandom();
+
+    function r() {
+        return equal ? oneRandom : getRandom();
+    }
+
+    return {
+        left: {
+            top: r(),
+            bottom: r(),
+        },
+        right: {
+            top: r(),
+            bottom: r(),
+        },
+        top: {
+            left: r(),
+            right: r(),
+        },
+        bottom: {
+            left: r(),
+            right: r(),
+        },
+    };
+}
+
+function makeShiftsWithTime(shifts, time, maxTime) {
+    return {
+        left: {
+            top: map(time, 0, maxTime, 0, shifts.left.top),
+            bottom: map(time, 0, maxTime, 0, shifts.left.bottom),
+        },
+        right: {
+            top: map(time, 0, maxTime, 0, shifts.right.top),
+            bottom: map(time, 0, maxTime, 0, shifts.right.bottom),
+        },
+        top: {
+            left: map(time, 0, maxTime, 0, shifts.top.left),
+            right: map(time, 0, maxTime, 0, shifts.top.right),
+        },
+        bottom: {
+            left: map(time, 0, maxTime, 0, shifts.bottom.left),
+            right: map(time, 0, maxTime, 0, shifts.bottom.right),
+        },
+    };
 }
 
 function map(num, frombottom, fromtop, tobottom, totop) {
@@ -77,15 +171,3 @@ function map(num, frombottom, fromtop, tobottom, totop) {
     a += tobottom;
     return a;
 }
-
-
-let start, previousTimeStamp;
-let done = false
-
-function step(timestamp) {
-  console.log(timestamp);
-  window.requestAnimationFrame(step);
-}
-
-window.requestAnimationFrame(step);
-
