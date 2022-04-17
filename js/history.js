465bfdb9299c288a77170542ce0eaa1fdf4febf7
Oleksandr Panov <oleksandr.panov@styrax.cz>
2022-04-17 19:01:35 +0200

    adding history items with links

--- js/history.js
+++ js/history.js
@@ -1,36 +1,5 @@
 'use strict';
 
-// const imagesHistory = {
-//     imageDatas: [],
-//     current: -1,
-//     add() {
-//         if (this.current !== this.imageDatas.length - 1) {
-//             //add after ctrl+z
-//             this.imageDatas.length = this.current + 1;
-//         }
-//         this.current++;
-//         this.imageDatas.push(ctx.getImageData(0, 0, CONFIG.canvasWidth, CONFIG.canvasHeight));
-//     },
-//     back() {
-//         if (this.current <= 0) {
-//             throw new Error('Cannot go back from first position');
-//         }
-//         this.current--;
-//         this.draw();
-//     },
-//     forward() {
-//         if (this.current >= this.imageDatas.length - 1) {
-//             throw new Error('Cannot go forward from last position');
-//         }
-//         this.current++;
-//         this.draw();
-//     },
-//     draw() {
-//         const imgData = this.imageDatas[this.current];
-//         ctx.putImageData(imgData, CONFIG.canvasWidth - imgData.width, CONFIG.canvasHeight - imgData.height);
-//     },
-// };
-
 const imagesHistory = {
     imageDatas: [],
     current: -1,
@@ -38,10 +7,10 @@ const imagesHistory = {
     add() {
         const currentHash = window.location.hash;
         const imgData = ctx.getImageData(0, 0, CONFIG.canvasWidth, CONFIG.canvasHeight);
-        const parent = (currentHash && currentHash.length > 2) ? currentHash.substring(1) : null;
+        const parent = (currentHash && currentHash.length > 2 && this.imageDatas[currentHash.substring(1)]) ? currentHash.substring(1) : null;
         this.current++;
         const newHash = 'plus' + this.current;
-        this.imageDatas[newHash] = new HistoryItem(imgData, parent);
+        this.imageDatas[newHash] = new HistoryItem(newHash, imgData, parent);
         window.location.hash = newHash;
     }
 };
@@ -56,20 +25,43 @@ window.onhashchange = function() {
 }
 
 class HistoryItem {
+    /** @type {string} */ key;
     /** @type {ImageData} */ imageData;
     /** @type {HistoryItem?} */ parent;
     // /** @type {HistoryItem[]} */ children; // .filter(parent === key);
     /** @type {HTMLElement} */ root;
     /** @type {boolean} */ opened = true;
 
-    constructor (imageData, parent) {
+    constructor (key, imageData, parent, active = true) {
+        this.key = key;
         this.imageData = imageData;
         this.parent = parent;
-        // this.children = [];
-        this.root = this.render(true);
+        this.render(active);
     }
 
     render(active) {
-        //write later
+        const mainDiv = document.createElement('div');
+        mainDiv.classList.add('item');
+        if (active) mainDiv.classList.add('active');
+        const contentDiv = document.createElement('div');
+        contentDiv.classList.add('content');
+        const childrenDiv = document.createElement('div');
+        childrenDiv.classList.add('children');
+        mainDiv.appendChild(contentDiv);
+        mainDiv.appendChild(childrenDiv);
+        mainDiv.contentDiv = contentDiv;
+        mainDiv.childrenDiv = childrenDiv;
+        this.root = mainDiv;
+
+        //tofo fix later
+        contentDiv.innerHTML = `<a href="#${this.key}">${this.key}</a>`;
+        console.log(`<a href="#${this.key}">${this.key}</a>`);
+
+        if (this.parent) {
+            const parentEl = imagesHistory.imageDatas[this.parent];
+            parentEl.root.childrenDiv.appendChild(this.root);
+        } else {
+            document.querySelector('#history > div').appendChild(this.root);
+        }
     }
 }
