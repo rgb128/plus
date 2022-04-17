26d07cc4260cc7df30ab05558e2bb5051eee282f
oldseptum <68962124+oldseptum@users.noreply.github.com>
2022-04-17 14:19:45 +0300

    Дизайн кнопок, новые цвета

--- js/colors.js
+++ js/colors.js
@@ -21,6 +21,18 @@ const colorPalettes = [
         name: 'Der Wanderer über dem Nebelmeer',
         colors: ['#DAE4EE','#EDEBE8','#B9BECB','#4C6C74','#939288','#6D7984','#868285','#646057','#574F4E','#1B1A1F'],
     },
+    {
+        name: 'Солярис 1972',
+        colors: ['#E1150D','#DCCF15','#B0A816','#64A6CE','#2467A4','#962620','#E6DAD2','#1E3254','#100709','#445B52'],
+    },
+    {
+        name: 'Pulp Fiction',
+        colors: ['#E6BA45','#DF302F','#905B3A','#4E76A3','#D2D1D7','#542C20','#99A994','#363F57','#5C5D50','#141210'],
+    },
+    {
+        name: 'Бутерброд',
+        colors: ['#3C6C04','#BAC8DC','#A5B3D3','#552F18','#D9D7D5','#615C2C','#B7B0A9','#A0A6AA','#7C7771','#2F261C'],
+    },
 ];
 
 function getColorPalette() {
