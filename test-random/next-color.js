a5fdaa7cb28edff987f74917a323418f97d7bae4
Oleksandr Panov <oleksandr.panov@styrax.cz>
2022-05-01 15:31:03 +0200

    add: no repeats

--- test-random/next-color.js
+++ test-random/next-color.js
@@ -1,5 +1,11 @@
 function nextColor(colorPalette) {
-    return randomEntity(colorPalette.colors);
+    const colors = colorPalette.colors;
+    let colorIndex = randomInt(colors.length - 1);
+    if (colorIndex === colorPalette.previousColor) {
+        colorIndex = colors.length - 1;
+    }
+    colorPalette.previousColor = colorIndex;
+    return colors[colorIndex];
 }
 
 function randomInt(max) {
